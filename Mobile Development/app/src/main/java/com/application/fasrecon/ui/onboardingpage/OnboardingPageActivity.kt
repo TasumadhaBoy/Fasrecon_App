79139152b01e37eb6d67f2c20ec72602791c2bdc
Tasumadha <d1041221037@student.untan.ac.id>
2024-11-16 23:10:19 +0700

    feat: set motion layout in portrait

--- Mobile Development/app/src/main/java/com/application/fasrecon/ui/onboardingpage/OnboardingPageActivity.kt
+++ Mobile Development/app/src/main/java/com/application/fasrecon/ui/onboardingpage/OnboardingPageActivity.kt
@@ -6,12 +6,25 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.application.fasrecon.R
+import com.application.fasrecon.databinding.ActivityOnboardingPageBinding
 
-class onboardingPageActivity : AppCompatActivity() {
+class OnboardingPageActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityOnboardingPageBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_onboarding_page)
+        binding = ActivityOnboardingPageBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnLoginOnboardingPage.setOnClickListener {
+
+        }
+
+        binding.btnSignupOnboardingPage.setOnClickListener {
+
+        }
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
