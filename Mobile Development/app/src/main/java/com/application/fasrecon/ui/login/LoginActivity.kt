8c7095be84ea3bdd80f46524f202005cafc80952
Tasumadha <d1041221037@student.untan.ac.id>
2024-11-20 07:12:47 +0700

    feat: email and password login checker

--- Mobile Development/app/src/main/java/com/application/fasrecon/ui/login/LoginActivity.kt
+++ Mobile Development/app/src/main/java/com/application/fasrecon/ui/login/LoginActivity.kt
@@ -2,13 +2,15 @@ package com.application.fasrecon.ui.login
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.application.fasrecon.MainActivity
+import com.application.fasrecon.R
 import com.application.fasrecon.databinding.ActivityLoginBinding
 import com.application.fasrecon.ui.forgotpassword.ForgotPassword
 import com.application.fasrecon.ui.register.RegisterActivity
 
-class LoginActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity(), View.OnClickListener {
 
     private lateinit var binding: ActivityLoginBinding
 
@@ -17,19 +19,70 @@ class LoginActivity : AppCompatActivity() {
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.forgotPassword.setOnClickListener {
-            val moveToForgotPasswordPage = Intent(this@LoginActivity, ForgotPassword::class.java)
-            startActivity(moveToForgotPasswordPage)
-        }
-        binding.btnLogin.setOnClickListener {
-            val moveToLoginPage = Intent(this@LoginActivity, MainActivity::class.java)
-            startActivity(moveToLoginPage)
-        }
-        binding.registerNavigation.setOnClickListener {
-            val moveToRegisterPage = Intent(this@LoginActivity, RegisterActivity::class.java)
-            startActivity(moveToRegisterPage)
-        }
+        binding.btnLogin.setOnClickListener(this)
+        binding.registerNavigation.setOnClickListener(this)
+        binding.forgotPassword.setOnClickListener(this)
 
         supportActionBar?.hide()
+    }
+
+    override fun onClick(v: View) {
+        when (v.id) {
+            R.id.btn_login -> {
+                val email = binding.emailLoginInput.text.toString().trim()
+                val password = binding.passwordLoginInput.text.toString().trim()
+
+                if (email.isEmpty()) {
+                    binding.emailLoginInput.setError(
+                        resources.getString(R.string.field_email),
+                        null
+                    )
+                    binding.emailLoginInput.requestFocus()
+                    return
+                }
+                if (!android.util.Patterns.EMAIL_ADDRESS.matcher(email)
+                        .matches() && email.isNotBlank()
+                ) {
+                    binding.emailLoginInput.setError(
+                        resources.getString(R.string.format_email),
+                        null
+                    )
+                    return
+                }
+                if (password.isEmpty()) {
+                    binding.passwordLoginInput.setError(
+                        resources.getString(R.string.field_password),
+                        null
+                    )
+                    binding.passwordLoginInput.requestFocus()
+                    return
+                }
+                if (password.length < 8) {
+                    binding.passwordLoginInput.setError(
+                        resources.getString(R.string.format_password),
+                        null
+                    )
+                    return
+                }
+
+                // loginViewModel.loginAccount(email, password)
+                val moveToLoginPage =
+                    Intent(this@LoginActivity, MainActivity::class.java)
+                startActivity(moveToLoginPage)
+            }
+
+            R.id.register_navigation -> {
+                val moveToRegisterPage =
+                    Intent(this@LoginActivity, RegisterActivity::class.java)
+                startActivity(moveToRegisterPage)
+            }
+
+            R.id.forgot_password -> {
+                val moveToForgotPasswordPage =
+                    Intent(this@LoginActivity, ForgotPassword::class.java)
+                startActivity(moveToForgotPasswordPage)
+            }
+        }
+
     }
 }
