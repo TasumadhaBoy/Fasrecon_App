47687e5536a358955584a0a99f85dc8abe88f44e
Tasumadha <d1041221037@student.untan.ac.id>
2024-11-27 15:49:41 +0700

    style: Set item chatbot message layout

--- Mobile Development/app/src/main/java/com/application/fasrecon/ui/historychatbot/HistoryChatbotViewModel.kt
+++ Mobile Development/app/src/main/java/com/application/fasrecon/ui/historychatbot/HistoryChatbotViewModel.kt
@@ -1,10 +1,10 @@
-package com.application.fasrecon.ui.chatbot
+package com.application.fasrecon.ui.historychatbot
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class ChatbotViewModel : ViewModel() {
+class HistoryChatbotViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
         value = "This is notifications Fragment"
