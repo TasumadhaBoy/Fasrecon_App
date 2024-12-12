1f566d44d67a6e4565e4fd43ad2adacd6bc27f14
XpieX <dzauqi8@gmail.com>
2024-12-12 21:06:53 +0700

    timeout

--- Mobile Development/app/src/main/java/com/application/fasrecon/data/remote/retrofit/ApiConfig.kt
+++ Mobile Development/app/src/main/java/com/application/fasrecon/data/remote/retrofit/ApiConfig.kt
@@ -5,6 +5,7 @@ import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 class ApiConfig {
     companion object {
@@ -13,7 +14,12 @@ class ApiConfig {
             val loggingInterceptor =
                 HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
 
-            val client = OkHttpClient.Builder().addInterceptor(loggingInterceptor).build()
+            val client = OkHttpClient.Builder().addInterceptor(loggingInterceptor)
+                .connectTimeout(30, TimeUnit.SECONDS)
+                .writeTimeout(30, TimeUnit.SECONDS)
+                .readTimeout(30, TimeUnit.SECONDS)
+                .retryOnConnectionFailure(true)
+                .build()
 
             val retrofit = Retrofit.Builder().baseUrl(BuildConfig.BASE_URL)
                 .addConverterFactory(GsonConverterFactory.create()).client(client).build()
